3356c16182b48c78b312aa8077c0c10860f9fc84
Mustafa <mustafa.mahmoud.fci@gmail.com>
2024-05-28 12:32:29 +0300

    Add livewire todo crud

---
+++ resources/views/livewire/todo.blade.php
@@ -0,0 +1,6 @@
+<div id="content" class="mx-auto" style="max-width:500px;">
+    @include('livewire.includes.todo-create')
+    @include('livewire.includes.search-box')
+    @include('livewire.includes.todo-list')
+
+</div>
